e71cc15e7a30f721c4d74d2471db8a0aaccce0a5
honlnm <nicole.m.honl@gmail.com>
2023-10-10 16:04:55 -0500

    finished exercise

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, flash, request
+from flask import Flask, render_template, redirect, flash, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -8,14 +8,18 @@ app.config['SECRET_KEY'] = "Pineapple24"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = list()
-
 @app.route('/')
 def home_page():
     return render_template("home.html", survey = survey)
 
+@app.route('/start', methods=["POST"])
+def set_session():
+    session['responses'] = list()
+    return redirect('/questions/0')
+
 @app.route('/questions/<question_num>')
 def get_next_question(question_num):
+    responses = session.get('responses')
     if int(len(responses)) == int(len(survey.questions)):
         return redirect ('/thanks')
     if int(question_num) != int(len(responses)):
@@ -27,8 +31,10 @@ def get_next_question(question_num):
 @app.route("/answers", methods=["POST"])
 def handle_question():
     choice = request.form['answer']
-    responses.append(choice)
-    return redirect(f'/questions/{len(responses)}')
+    list = session['responses']
+    list.append(choice)
+    session['responses'] = list
+    return redirect(f'/questions/{len(list)}')
 
 @app.route("/thanks")
 def thank_you():
